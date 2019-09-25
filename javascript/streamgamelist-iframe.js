665ba9af67ac180706b912a899cb97a75e160fd1
ririo <ririomail@gmail.com>
2019-09-25 22:33:00 +0900

    change streamGameList, add iframe

---
+++ javascript/streamgamelist-iframe.js
@@ -0,0 +1,18 @@
+// 未使用
+
+
+
+// const gamelist = document.querySelectorAll("dd a");
+// console.log({gamelist});
+// var iframeArray = [];
+// iframeArray = [
+//     "Deltarune.html", 
+//     "DestinyChild.html",
+// ];
+// console.log({iframeArray});
+// var iframe = document.getElementById("iframe");
+// var iframe_src = iframe.getAttribute("src");
+// console.log({iframe_src});
+// gamelist[n].addEventListener('click', function(){
+//     iframe_src = `./streamGameList/${iframeArray[n]}`;
+// }, false);
