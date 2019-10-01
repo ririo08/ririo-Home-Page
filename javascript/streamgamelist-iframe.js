07548b68af9d187c169432c32f176b74f2d80a87
ririo <ririomail@gmail.com>
2019-10-01 17:35:46 +0900

    add "jump to iframe"

--- javascript/streamgamelist-iframe.js
+++ javascript/streamgamelist-iframe.js
@@ -1,18 +1,9 @@
-// 未使用
+var jump = function(){
+    location.hash = "";
+    location.hash = "iframe";
+}
 
+document.querySelectorAll("dd").forEach(function(foo){
+    foo.addEventListener('click', jump);
+});
 
-
-// const gamelist = document.querySelectorAll("dd a");
-// console.log({gamelist});
-// var iframeArray = [];
-// iframeArray = [
-//     "Deltarune.html", 
-//     "DestinyChild.html",
-// ];
-// console.log({iframeArray});
-// var iframe = document.getElementById("iframe");
-// var iframe_src = iframe.getAttribute("src");
-// console.log({iframe_src});
-// gamelist[n].addEventListener('click', function(){
-//     iframe_src = `./streamGameList/${iframeArray[n]}`;
-// }, false);
