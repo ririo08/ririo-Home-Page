84554cf395890057d81de41f86014dd052cb42c3
ririo08 <ririomail@gmail.com>
2019-10-26 10:26:31 +0900

    実況動画リストを更新しました！

---
+++ javascript/letsPlayList-titlecount.js
@@ -0,0 +1,3 @@
+const letsPlayListJsTarget = document.getElementById('letsPlayList-jstarget');
+let gameTitleCount = document.querySelectorAll('.listTable-title').length;
+letsPlayListJsTarget.innerHTML = gameTitleCount;
